c0028cd9e96a3948e1d4f67ce698f3331d3f756e
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-11 08:24:41 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,11 @@ class ProductsController < ApplicationController
       format.html { render :show }
       format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
     end
+  end 
+
+  def product_data
+    post = Post.find(params[:id])
+    render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])
   end
 
   private
